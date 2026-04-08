4ba677a729ae9ef1a0f1d20f69c5c644f4c2059c
Matthew Li <matthewrli@ucsb.edu>
2026-04-07 18:45:35 -0700

    equal and hashcode test changes

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,8 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,8 +21,65 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_sameObject_returnsTrue() {
+        assertTrue(team.equals(team));
+    }
+
+    @Test
+    public void equals_differentClass_returnsFalse() {
+        assertFalse(team.equals("not a team"));
+        assertFalse(team.equals(new Object()));
+    }
+
+    @Test
+    public void equals_sameNameAndMembers_returnsTrue() {
+        Team a = new Team("red");
+        Team b = new Team("red");
+        a.addMember("alice");
+        b.addMember("alice");
+        assertTrue(a.equals(b));
+    }
+
+    @Test
+    public void equals_sameNameDifferentMembers_returnsFalse() {
+        Team a = new Team("red");
+        a.addMember("alice");
+        Team b = new Team("red");
+        b.addMember("bob");
+        assertFalse(a.equals(b));
+    }
+
+    @Test
+    public void equals_differentName_returnsFalse() {
+        Team a = new Team("red");
+        Team b = new Team("blue");
+        assertFalse(a.equals(b));
+    }
+
+    @Test
+    public void hashCode_sameContents_returnsSameValue() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("bar");
+        Team t2 = new Team();
+        t2.setName("foo");
+        t2.addMember("bar");
+        assertEquals(t1.hashCode(), t2.hashCode());
+    }
+
+    @Test
+    public void hashCode_matchesCombinedValueForKnownTeam() {
+        Team t = new Team();
+        t.setName("foo");
+        t.addMember("bar");
+        int expectedResult = 130294;
+        assertEquals(expectedResult, t.hashCode());
+    }
 
 }
