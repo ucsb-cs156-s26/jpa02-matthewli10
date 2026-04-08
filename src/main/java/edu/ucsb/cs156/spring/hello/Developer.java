45ac83cdad2b6c8292bf326567373f1b27c3d1e3
Matthew Li <matthewrli@ucsb.edu>
2026-04-07 18:09:59 -0700

    Add tests for team members

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -35,13 +35,13 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("s26-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s26-08");
+        team.addMember("Matthew");
+        team.addMember("Anjali");
+        team.addMember("Christian");
+        team.addMember("Ria");
+        team.addMember("Taedon");
+        team.addMember("Wendy");
         return team;
     }
 }
